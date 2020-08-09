1519f48c756df5b5b20f4f53c8496a295d0c0557
Hiroaki Kobayashi <kobtea9696@gmail.com>
2020-08-10 02:06:26 +0900

    support major version tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-version=${INPUT_PREFIX}$(cat VERSION)${INPUT_SUFFIX}
+version=${INPUT_PREFIX}$($INPUT_ONLY_MAJOR_VERSION && (cat VERSION | cut -d. -f1) || cat VERSION)${INPUT_SUFFIX}
 git config user.name  || git config --local user.name $(git log -1 --pretty=format:'%an')
 git config user.email || git config --local user.email $(git log -1 --pretty=format:'%ae')
 git ls-remote --exit-code --tags origin $version > /dev/null 2>&1 || (git tag -a $version -m "release $version" && git push origin $version)
