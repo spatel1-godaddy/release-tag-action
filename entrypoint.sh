a7bcd944d4c9e0ce487a2710c6616931d4b94f96
Hiroaki Kobayashi <kobtea9696@gmail.com>
2020-08-10 02:56:09 +0900

    support custom message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 version=${INPUT_PREFIX}$($INPUT_ONLY_MAJOR_VERSION && (cat VERSION | cut -d. -f1) || cat VERSION)${INPUT_SUFFIX}
+message=$(echo $INPUT_MESSAGE | sed s/__VERSION__/$version/g)
 git config user.name  || git config --local user.name $(git log -1 --pretty=format:'%an')
 git config user.email || git config --local user.email $(git log -1 --pretty=format:'%ae')
 if $INPUT_FORCE; then
-    git tag -af $version -m "release $version" && git push -f origin $version
+    git tag -af $version -m "$message" && git push -f origin $version
 else
-    git ls-remote --exit-code --tags origin $version > /dev/null 2>&1 || (git tag -a $version -m "release $version" && git push origin $version)
+    git ls-remote --exit-code --tags origin $version > /dev/null 2>&1 || (git tag -a $version -m "$message" && git push origin $version)
 fi
 echo "::set-output name=tag::$version"
