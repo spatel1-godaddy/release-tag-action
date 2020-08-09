8140dee04fd4ab9f9208bce96bad220ae4044e44
Hiroaki Kobayashi <kobtea9696@gmail.com>
2020-08-10 01:35:26 +0900

    set output

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,4 @@ version=${INPUT_PREFIX}$(cat VERSION)${INPUT_SUFFIX}
 git config user.name  || git config --local user.name $(git log -1 --pretty=format:'%an')
 git config user.email || git config --local user.email $(git log -1 --pretty=format:'%ae')
 git ls-remote --exit-code --tags origin $version > /dev/null 2>&1 || (git tag -a $version -m "release $version" && git push origin $version)
+echo "::set-output name=tag::$version"
