0a9f6200c62578c4b97c347d2833a8e83f65b5e2
spatel1 <spatel1@godaddy.com>
2022-09-13 19:05:36 -0500

    add rpm build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
-version=${INPUT_PREFIX}$($INPUT_ONLY_MAJOR_VERSION && (cat VERSION | cut -d. -f1) || cat VERSION)${INPUT_SUFFIX}
-message=$(echo $INPUT_MESSAGE | sed s/__VERSION__/$version/g)
+
+# Put version tag
 git config --global --add safe.directory /github/workspace
+if [ $( git rev-parse --abbrev-ref HEAD )  = 'main' ]; then
+    INPUT_PREFIX='v'
+elif  [ $( git rev-parse --abbrev-ref HEAD )  = 'unstable' ]; then
+    INPUT_PREFIX='u'
+fi
+
+version=${INPUT_PREFIX}$(cat VERSION)${INPUT_SUFFIX}
+message=$(echo $INPUT_MESSAGE | sed s/__VERSION__/$version/g)
 git config user.name $(git log -1 --pretty=format:'%an')
 git config user.email $(git log -1 --pretty=format:'%ae')
 if $INPUT_FORCE; then
@@ -9,4 +17,9 @@ if $INPUT_FORCE; then
 else
     git ls-remote --exit-code --tags origin $version > /dev/null 2>&1 || (git tag -a $version -m "$message" && git push origin $version)
 fi
-echo "::set-output name=tag::$version"
+
+# Build RPM module
+rpmdev-setuptree
+cp -r /github/workspace/* /root/rpmbuild/SOURCES/
+
+rpmbuild -bb  /github/workspace/config.spec
